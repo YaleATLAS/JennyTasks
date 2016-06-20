016c510cc24a8f0a9d55f9f4205d6d21002074d6
Ubuntu <ubuntu@ip-172-31-29-106.eu-central-1.compute.internal>
2016-06-20 16:34:04 +0000

    complete task 2

---
+++ task2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from root_numpy import root2array
+import pandas as pd
+
+ttbar = root2array('ttbar.root')
+qcd = root2array('qcd.root')
+
+tdf = pd.DataFrame(ttbar)
+qdf = pd.DataFrame(qcd)
+
+matplotlib.rcParams.update({'font.size': 16})
+fig = plt.figure(figsize=(11.69, 8.27), dpi=100)
+bins = np.linspace(0, 10, num=11)
+_ = plt.hist([ttbar['NJet'], qcd['NJet']], stacked=True, normed=False, color = ['green','red'], bins=bins, histtype='stepfilled', weights=[ttbar['EventWeight'], qcd['EventWeight']], alpha=0.5, label=[r'$t\overline{t}$', 'QCD'], linewidth=2)
+plt.xlabel('NJet')
+plt.yscale('log')
+plt.ylabel('Number of Events')
+plt.legend()
+plt.plot()
+plt.savefig('task2.pdf')
