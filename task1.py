de0127eb95d649c2edb84741d1513c5781f24ec1
Ubuntu <ubuntu@ip-172-31-29-106.eu-central-1.compute.internal>
2016-06-20 10:27:11 +0000

    get four vectors and etas

--- task1.py
+++ task1.py
@@ -1,6 +1,9 @@
 import numpy as np
-from root_numpy import root2array
+from root_numpy import root2array, root2rec
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from rootpy.vector import LorentzVector
 
 ttbar = root2array('ttbar.root')
 
@@ -22,11 +25,20 @@ df_flat = pd.DataFrame({k: flatten(c) for k, c in jet_df.iteritems()})
 print ("{:.2f}".format(float(len(df_flat))/len(jet_df)))
 
 #print the number of events that do not pass the muon trigger (triggerIsoMu24 branch = False)
-muon_df = df[[key for key in df.keys() if key.startswith('trigger')]]
+muont_df = df[[key for key in df.keys() if key.startswith('trigger')]]
 count = 0
-for bool in muon_df.values:
+for bool in muont_df.values:
 	if bool==False:
 		count+=1
 print count
 
 #construct the four-vector of all jets
+df['Jet_4V'] = [map(lambda args: LorentzVector(*args), zip(px, py, pz, e)) for (_, (px, py, pz, e)) in df[['Jet_Px', 'Jet_Py', 'Jet_Pz', 'Jet_E']].iterrows()]
+
+#use the four-vectors to get the jet etas
+Jet_Eta = np.array(float)
+for jt in df['Jet_4V']:
+	for n in range (0,len(jt)-1):
+		Jet_Eta = np.append(Jet_Eta, jt[n].eta())
+
+
